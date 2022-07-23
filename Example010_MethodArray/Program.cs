3f6754e9dd93308cce12a210f1312946aa4a3e0a
Staxxx <sersersin@gmail.com>
2022-07-24 00:01:25 +0300

    add 8-11

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = {11,211,211,41,15,61,17,18,19 };
+
+int n = array.Length; // возвращает длинну массива
+
+int find = 211;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+     Console.WriteLine(index);
+     break;   
+    }
+    //index = index +1;
+    index++;
+
+}
